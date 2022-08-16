ae17b01d0d53a9acded2af1b8b984aee580cf143
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-16 14:15:13 +0700

    Задачи

---
+++ Задача_15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+string answer = "нет";
+
+if (0 < number && number< 8)
+{
+    if (number == 6 || number == 7)
+    {
+        answer = "да";
+    }
+    Console.WriteLine(answer);
+}
+else
+{
+    Console.WriteLine("Дня недели с таким номером не существует.");
+}
