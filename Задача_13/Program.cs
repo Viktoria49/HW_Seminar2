b8f3df1c4cd4441dcf100d092789b7ff55a5639e
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-16 14:42:16 +0700

    Задачи

---
+++ Задача_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number / 100 % 10;
+
+if (result>0)
+{
+    Console.WriteLine($"Третий разряд числа {number} равен = {result}");
+}
+else
+{
+    if (number/1000 > 0)
+    {
+        Console.WriteLine($"Третий разряд числа {number} равен = 0");
+    }
+    else
+    {
+        Console.WriteLine($"У числа {number} третьего разряда нет");
+    }
+}
