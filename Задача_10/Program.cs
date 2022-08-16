c644a7dbb1b1ed71bfb2507e925195ad79a8b7d6
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-16 13:24:35 +0700

    Задача_10

---
+++ Задача_10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number / 10 % 10;
+Console.WriteLine($"Вторая цифра числа {number} равна = {result}");
